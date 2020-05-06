077f297b0f387ff63817c750c12188874a45d7e1
ferfykins <ferfykins@gmail.com>
2020-05-05 20:52:12 -0400

    Nothing New2

--- src/com/company/MathExceptionIfElse.java
+++ src/com/company/MathExceptionIfElse.java
@@ -8,9 +8,7 @@ public class MathExceptionIfElse {
 
     public static void main(String[] args) {
 
-
-        System.out.println("Test");
-        System.out.println("Test2");
+        System.out.println("Test New");
 
         mainMenu();
 
